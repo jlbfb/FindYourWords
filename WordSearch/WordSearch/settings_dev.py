c7c86afcd00b2df6070b67ec0c7e7ae4a2522d23
jlbfb <38572799+jlbfb@users.noreply.github.com>
2020-11-16 20:32:45 -0700

    Update settings_dev.py

--- WordSearch/WordSearch/settings_dev.py
+++ WordSearch/WordSearch/settings_dev.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from .fyw_secrets import secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,7 @@ STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*%0pjk)eq_7fy#2mz65k%h-qsd6y2905%^td)ev9c9ri4gq70a'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
