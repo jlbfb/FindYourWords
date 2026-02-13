9d50e366b69d60e405640845c964b936fb14a118
jlbfb <38572799+jlbfb@users.noreply.github.com>
2026-02-13 13:20:26 -0700

    Commit to update wsgi file to pull DJANGO_SETTINGS_MODULE from .env file using django-environ; updated default for environment variable from settings to settings_prod.

--- WordSearch/WordSearch/wsgi.py
+++ WordSearch/WordSearch/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WordSearch.settings")
+os.environ.setdefault(env("DJANGO_SETTINGS_MODULE"), "WordSearch.settings_prod")
 
 application = get_wsgi_application()
